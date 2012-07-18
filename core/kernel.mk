5baa54fe17290d5f0791efe6d83d73ea8b161680
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2012-07-18 15:02:41 -0400

    kernel: allow to specify custom toolchain for kernel
    Example:
    TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7.2
    
    Change-Id: If89a44e4a090396c5a8076e295cbb6b6adf6ba2c

--- core/kernel.mk
+++ core/kernel.mk
@@ -94,7 +94,11 @@ ifeq ($(TARGET_ARCH),arm)
       # Check that the executable is here.
       ccache := $(strip $(wildcard $(ccache)))
     endif
-    ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
+      ifneq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),)
+       ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilt/linux-x86/toolchain/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
+      else
+       ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
+      endif
     ccache = 
 endif
 
