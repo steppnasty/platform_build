7cdce998e25a308ee7062884a0de455a2ef45512
Rob Beane <robbeane@gmail.com>
2012-08-03 23:49:14 -0400

    kernel.mk: Allow building with AOSP toolchain by default
    The AOSP toolchain should be good enough for most kernels these days
    
    Change-Id: If6aadd891ebb515a16d36ddbb22b611977b66459

--- core/kernel.mk
+++ core/kernel.mk
@@ -97,7 +97,7 @@ ifeq ($(TARGET_ARCH),arm)
       ifneq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),)
        ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilt/linux-x86/toolchain/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
       else
-       ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
+       ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-"
       endif
     ccache = 
 endif
